ee3617c477d4158f359b1fdf862a503fd91b48cc
Pierre-Luc Dupont <pldupont@acquisio.com>
2016-04-19 09:59:26 -0400

    Add color to my test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,31 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
-Minitest::Reporters.use!
 
+module Minitest
+  module Reporters
+    class AwesomeReporter < DefaultReporter
+      GREEN = '1;32'
+      RED = '1;31'
+
+      def color_up(string, color)
+        color? ? "\e\[#{ color }m#{ string }#{ ANSI::Code::ENDCODE }" : string
+      end
+
+      def red(string)
+        color_up(string, RED)
+      end
+
+      def green(string)
+        color_up(string, GREEN)
+      end
+    end
+  end
+end
+
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::AwesomeReporter.new(reporter_options)]
+	
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
