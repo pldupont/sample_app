cb53389de612c016181d924798c4ab674c60dd5f
Pierre-Luc Dupont <pldupont@acquisio.com>
2016-04-30 17:16:45 -0400

    Up to chapter 9.3.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,9 +14,9 @@ module SessionsHelper
   # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      @current_user ||= User.find_by_id(user_id)
     elsif (user_id = cookies.signed[:user_id])
-	  #raise
+      #raise
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -24,12 +24,17 @@ module SessionsHelper
       end
     end
   end
-  
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
-  
+
   # Forgets a persistent session.
   def forget(user)
     user.forget
@@ -42,5 +47,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, opts = {})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{opts[:size]}"
+    puts gravatar_url
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
