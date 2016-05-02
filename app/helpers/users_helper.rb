7c4f094115bc2e0fa23176c3a72f745b1c34451c
Pierre-Luc Dupont <pldupont@acquisio.com>
2016-05-02 10:21:54 -0400

    Completed chapter 9

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,6 @@ module UsersHelper
   def gravatar_for(user, opts = {})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{opts[:size]}"
-    puts gravatar_url
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
