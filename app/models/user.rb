4c997cebab12afc8a19d6af8daab8536b950ddb0
Pierre-Luc Dupont <pldupont@acquisio.com>
2016-05-02 15:06:29 -0400

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,17 @@ class User < ActiveRecord::Base
   has_secure_password
 
   attr_accessor :remember_token, :activation_token
-  validates :name, presence:true, length: { maximum: 50, minimum: 5 }
-  validates :email, presence:true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: {maximum: 50, minimum: 5}
+  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                            BCrypt::Engine.cost
+        BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-  
+
 
   # Returns a random token.
   def User.new_token
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
-  end  
+  end
 
   # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
@@ -38,6 +38,17 @@ class User < ActiveRecord::Base
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # Activates an account.
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
