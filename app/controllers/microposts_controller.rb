ccc4e1b8aabb0cc46518ee4fd1094a1115b291be
Pierre-Luc Dupont <pldupont@acquisio.com>
2016-05-04 18:17:11 -0400

    Adding integration test on micropost management

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -9,15 +10,24 @@ class MicropostsController < ApplicationController
     else
       @feed_items = [] # current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
+      #redirect_to request.referrer || root_url
     end
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by_id(params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -8,7 +8,7 @@ class MicropostsControllerTest < ActionController::TestCase
 
   test "should redirect create when not logged in" do
     assert_no_difference 'Micropost.count' do
-      post :create, micropost: { content: "Lorem ipsum" }
+      post :create, micropost: {content: "Lorem ipsum"}
     end
     assert_redirected_to login_url
   end
@@ -18,5 +18,14 @@ class MicropostsControllerTest < ActionController::TestCase
       delete :destroy, id: @micropost
     end
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, id: micropost
+    end
+    assert_redirected_to root_url
   end
 end
