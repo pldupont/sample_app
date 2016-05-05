c04a8689dbc62df545baa3cff05415d0aa81cb93
Pierre-Luc Dupont <pldupont@acquisio.com>
2016-05-05 10:16:22 -0400

    Displaying followers and following on the Home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ActiveRecord::Base
   before_create :create_activation_digest
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+           foreign_key: "follower_id",
+           dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+           foreign_key: "followed_id",
+           dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
   validates :name, presence: true, length: {maximum: 50, minimum: 5}
@@ -72,6 +80,18 @@ class User < ActiveRecord::Base
   # See "Following users" for the full implementation.
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
